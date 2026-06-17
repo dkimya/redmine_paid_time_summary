8ac977b92125732f5ab548d4d4f883d89772fe25
Davood <d.kimya@yahoo.com>
2026-06-16 18:10:40 -0700

    Namespace hook listener for Zeitwerk

--- lib/redmine_paid_time_summary/hooks.rb
+++ lib/redmine_paid_time_summary/hooks.rb
@@ -1,6 +1,8 @@
 require_relative '../../app/models/redmine_paid_time_summary'
 
-class RedminePaidTimeSummaryHook < Redmine::Hook::ViewListener
-  render_on :view_projects_show_left,
-            partial: 'redmine_paid_time_summary/project_overview_box'
+module RedminePaidTimeSummary
+  class Hooks < Redmine::Hook::ViewListener
+    render_on :view_projects_show_left,
+              partial: 'redmine_paid_time_summary/project_overview_box'
+  end
 end
